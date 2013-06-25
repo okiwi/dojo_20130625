c371b911a8888cfc27ef392a7d37fa0d18cb5f04
nostradamnit <me@nostradamnit.com>
2013-06-25 18:47:55 +0200

    starting dojo

---
+++ BowlingTests.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class BolwingTests(unittest.TestCase):
+    def setUp(self):
+        pass
+        
+    def test_blah(self):
+        pass
+        
+           
+if __name__ == "__main__":
+    unittest.main()
+    
+    
+    

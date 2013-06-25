b0aa4fdcfc0c795969a5b9de8e05ea30002a8b3d
nostradamnit <me@nostradamnit.com>
2013-06-25 19:05:50 +0200

    next step

--- BowlingTests.py
+++ BowlingTests.py
@@ -1,12 +1,15 @@
 import unittest
-
+from Bowling import Bowling
 
 class BolwingTests(unittest.TestCase):
     def setUp(self):
         pass
         
-    def test_blah(self):
-        pass
+    def test_can_bowl_2_times_4_equals_8(self):
+        game = Bowling()
+        game.roll(4)
+        game.roll(4)
+        self.assertEquals(8, game.score())
         
            
 if __name__ == "__main__":

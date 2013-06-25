9244447edc08c183f659cdf6ee828601ba38fc93
nostradamnit <me@nostradamnit.com>
2013-06-25 19:06:05 +0200

    next step

---
+++ Bowling.py
@@ -0,0 +1,11 @@
+class Bowling (object):
+    
+    def __init__(self):
+        self._score = 0
+    
+    def roll(self, pins):
+        self._score += pins
+        
+    def score(self):
+        return self._score    
+        
